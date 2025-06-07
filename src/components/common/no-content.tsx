ab4b2755c7be476cda51ce72ae8c1cbc1516f2d9
hijjoy <hijjoy@naver.com>
2025-06-07 22:27:51 +0900

    feat: NoContent 컴포넌트에 이미지 및 텍스트 추가

--- src/components/common/no-content.tsx
+++ src/components/common/no-content.tsx
@@ -1,9 +1,19 @@
+import MotionOpacity from "./motion-opacity";
 import Text from "./text";
+import Image from "next/image";
 
 export default function NoContent() {
   return (
-    <div className="flex flex-col h-full">
-      <Text className="text-sm text-gray-400">아직 글이 없어요</Text>
-    </div>
+    <MotionOpacity className="flex flex-col items-center py-1 gap-4">
+      <Image src="/images/쪼이.svg" alt="logo" width={400} height={400} />
+      <div>
+        <Text className="text-sm text-gray-400 text-center">
+          아직 글이 없어요
+        </Text>
+        <Text className="text-sm text-gray-400 text-center">
+          잠시만 기다려주세요 !
+        </Text>
+      </div>
+    </MotionOpacity>
   );
 }
