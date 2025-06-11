29e70d194347c121543dbeed64e2c23575c41fc2
hijjoy <hijjoy@naver.com>
2025-06-11 22:32:04 +0900

    feat: sitemap.ts 파일 추가

---
+++ src/app/sitemap.ts
@@ -0,0 +1,23 @@
+import { getPosts } from '@/domains/post/queries/get-posts';
+import { MetadataRoute } from 'next';
+
+export default async function Sitemap(): Promise<MetadataRoute.Sitemap> {
+  const posts = await getPosts();
+
+  const postEntries = posts.map((post) => ({
+    url: `${process.env.NEXT_PUBLIC_BASE_URL}/article/${post.slug}`,
+    lastModified: post.createdAt,
+  }));
+
+  return [
+    {
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}/about`,
+      lastModified: new Date(),
+    },
+    ...postEntries,
+  ];
+}
