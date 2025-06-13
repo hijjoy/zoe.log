225a5bdcc47892020877d88d21d01973b2e8e971
hijjoy <hijjoy@naver.com>
2025-06-13 15:53:32 +0900

    chore: @prisma/nextjs-monorepo-workaround-plugin 추가 및 설정 - 버셀 ssr 오류 수정

--- apps/blog/next.config.ts
+++ apps/blog/next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from 'next';
+import { PrismaPlugin } from '@prisma/nextjs-monorepo-workaround-plugin';
 
 const nextConfig: NextConfig = {
   /* config options here */
+
+  // vercel 배포를 위한 설정
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()];
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
