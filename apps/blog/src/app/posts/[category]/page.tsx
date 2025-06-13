86e2f2c599f5fa82aef0f651f96cac990dd1c9b0
hijjoy <hijjoy@naver.com>
2025-06-13 21:37:45 +0900

    feat: 카테고리 ISR

--- apps/blog/src/app/posts/[category]/page.tsx
+++ apps/blog/src/app/posts/[category]/page.tsx
@@ -1,6 +1,16 @@
+export const revalidate = 3600;
+
 import { CATEGORY, Category } from '@/domains/post/constants';
 import { notFound } from 'next/navigation';
 import PostsWithCategoryView from '@/domains/post/ui/views/posts-with-category-view';
+
+export async function generateStaticParams() {
+  // 카테고리 목록을 빌드 타임에 가져옴
+  // TODO:카테고리 스키마 변경후 db에서 가져오는 것으로 수정
+  return Object.keys(CATEGORY).map((category) => ({
+    category,
+  }));
+}
 
 interface Props {
   params: Promise<{
