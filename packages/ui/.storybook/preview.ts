e42ca543d02515842f08a0736d3cd3b3d63b14f1
hijjoy <hijjoy@naver.com>
2025-06-17 19:45:42 +0900

    feat: Storybook 및 Vitest 설정 추가

---
+++ packages/ui/.storybook/preview.ts
@@ -0,0 +1,32 @@
+import type { Preview } from '@storybook/nextjs';
+import '../styles/globals.css';
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+    nextjs: {
+      appDirectory: true,
+      navigation: {
+        pathname: '/posts',
+        query: {},
+      },
+    },
+    a11y: {
+      config: {
+        rules: [
+          {
+            id: 'color-contrast',
+            enabled: false,
+          },
+        ],
+      },
+    },
+  },
+};
+
+export default preview;
